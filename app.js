fccc163991f19363723e80ce12029c5a4d304d2d
FedGorunov <pinta.f14@gmail.com>
2018-06-25 00:31:31 +0700

    add method post to server

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const createTask = app.post('/todos',function(req, res){
+    let task =req.body.task;
+    // save task to BD  
+});
+
 app.get("/", function(req, res){
     console.log("Hello!");
     res.send("Привет!!");   
@@ -9,5 +14,5 @@ const port = 8080;
 
 app.listen(port, function(){   
     console.log(`Server started on port: ${port}`);
-} );
+});
  
