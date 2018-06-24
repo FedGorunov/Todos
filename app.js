2fb1b3796f4fcacf4c31a585dcf40172eebc24a1
FedGorunov <pinta.f14@gmail.com>
2018-06-24 21:11:41 +0700

    clean up code

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const app = express();
+
 app.get("/", function(req, res){
     console.log("Hello!");
-    res.send("Привет!!");
-    
+    res.send("Привет!!");   
 });
 const port = 8080;
 
-app.listen(port, function(){
-    
+app.listen(port, function(){   
     console.log(`Server started on port: ${port}`);
 } );
  
